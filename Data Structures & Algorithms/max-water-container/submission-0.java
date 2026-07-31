183f981986f308ede1e8d2583fc0e1f4be41c21a
Kushal Sharma <kushalsharmaworks@gmail.com>
2026-07-31 23:28:08 +0530

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxArea(int[] heights) {
+        //brute force approach
+        int maxArea = 0;
+        int area = 0;
+        for(int i=0;i<heights.length;i++)
+        {
+            for(int j=i+1;j<heights.length;j++)
+            {
+                area = (j-i) * (Math.min(heights[i],heights[j]));
+
+                if(area>maxArea) maxArea = area;
+            }
+        }
+        return maxArea;
+    }
+}
